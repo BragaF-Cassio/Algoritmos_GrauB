144cf759f0f4f77effa4a5c611722771f6dba90d
barthduda <116377444+barthduda@users.noreply.github.com>
2023-06-01 11:46:00 -0300

    Add files via upload

---
+++ src/Eletronicos.java
@@ -0,0 +1,32 @@
+package Entities;
+
+public class Eletronicos extends Produto {
+    private int anoDeLancamento;
+
+    public Eletronicos(String nome, String descricao, String marca, String cor, String categoria, double valor, int quantidade, int anoDeLancamento) {
+        super(nome, descricao, marca, cor, categoria, valor, quantidade);
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Eletronicos{" +
+                "anoDeLancamento=" + anoDeLancamento +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", marca='" + marca + '\'' +
+                ", cor='" + cor + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", valor=" + valor +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+}
