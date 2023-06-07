a49cc3037956278f4b5f84a7ab6514b3f436912e
lucasburghardt <134979232+lucasburghardt@users.noreply.github.com>
2023-06-07 12:17:53 -0300

    Add files via upload

---
+++ src/Eletrodomestico.java
@@ -0,0 +1,34 @@
+
+public class Eletrodomestico extends Produto{
+	private String eficienciaEnergetica;
+
+	public Eletrodomestico(String nome, String descricao, String marca, String cor, String categoria, double valor, int quantidade, int anoDeLancamento, String eficienciaEnergetica) {
+		super(nome, descricao, marca, cor, categoria, valor, quantidade);
+		this.eficienciaEnergetica = eficienciaEnergetica;
+	}
+
+	public String getEficienciaEnergetica() {
+		return eficienciaEnergetica;
+	}
+
+	public void setEficienciaEnergetica(String eficienciaEnergetica) {
+		this.eficienciaEnergetica = eficienciaEnergetica;
+	}
+
+	@Override
+	public String toString() {
+		return "Eletrodomestico:" + 
+				" Eficiencia Energetica=" + eficienciaEnergetica +
+				", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", marca='" + marca + '\'' +
+                ", cor='" + cor + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", valor=" + valor +
+                ", quantidade=" + quantidade +
+                '}';
+	}
+	
+	
+
+}
